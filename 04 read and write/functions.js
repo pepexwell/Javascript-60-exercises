3024bffe7a1453abf4d3b755e92b9054bf9009cb
Jose Augusto <pepexwell@gmail.com>
2017-07-06 15:49:02 -0400

    third commit
    Create four math operations

---
+++ 04 read and write/functions.js
@@ -0,0 +1,17 @@
+/**
+ * Created by pp on 7/6/2017.
+ */
+var data = 10;
+var string_1 = "Nombre full:" + " Jose Augusto";
+var bool_1 = 5>4;
+
+var num1 = 10;
+var num2 = 5;
+
+
+
+document.write(data + " " + string_1 + " " + bool_1 + "<br>");
+document.write(num1 + num2 + "<br>");
+document.write(num1 - num2 + "<br>");
+document.write(num1 * num2 + "<br>");
+document.write(num1 / num2 + "<br>");
