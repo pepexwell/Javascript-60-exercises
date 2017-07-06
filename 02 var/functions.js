cf32ab8f3f32e4f9dbb78eaab8706685a5e8cc81
Jose Augusto <pepexwell@gmail.com>
2017-07-06 15:24:34 -0400

    var creation
    Create three variables and play with them

---
+++ 02 var/functions.js
@@ -0,0 +1,10 @@
+/**
+ * Created by pp on 7/6/2017.
+ */
+
+var data = 10;
+var string_1 = "Nombre full:" + " Jose Augusto";
+
+var bool_1 = 5>4;
+
+document.write(data + " " + string_1 + " " + bool_1);
